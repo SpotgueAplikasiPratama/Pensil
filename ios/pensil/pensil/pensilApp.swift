4dc0f4e75085f128fdf955eac3419c35b2eb2e11
spotgue <Melvinvois@spotgue.com>
2023-12-04 16:01:20 +0700

    sync pensil live

---
+++ ios/pensil/pensil/pensilApp.swift
@@ -0,0 +1,17 @@
+//
+//  pensilApp.swift
+//  pensil
+//
+//  Created by spotgue on 01/09/23.
+//
+
+import SwiftUI
+
+@main
+struct pensilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
